38a4215dc778c533f80e95c8609134af8f7d2703
Roarkiner <dujour.lenaic@gmail.com>
2024-10-17 09:41:41 +0200

    First impl

---
+++ API/ServiceImpl/PersonneServiceImpl.cs
@@ -0,0 +1,16 @@
+﻿using Grpc.Core;
+using Personnes;
+using static Personnes.Personne;
+
+namespace API.ServiceImpl;
+
+public class PersonneServiceImpl: PersonneBase
+{
+    public async override Task<PersonneReponse> Ajouter(PersonneAjouterRequete request, ServerCallContext context)
+    {
+        return await Task.FromResult(new PersonneReponse
+        {
+            Id = 1
+        });    
+    }
+}
