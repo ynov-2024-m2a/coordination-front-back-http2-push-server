5814afe8da875306c54200c47a8b2a111de1e1c4
Roarkiner <dujour.lenaic@gmail.com>
2024-10-17 16:50:40 +0200

    v1

---
+++ API/ServiceImpl/MathServiceImpl.cs
@@ -0,0 +1,25 @@
+﻿
+namespace API.ServiceImpl;
+
+using Grpc.Core;
+using MathPerso;
+using System.Threading.Tasks;
+using static MathPerso.MathPerso;
+
+public class MathServiceImpl: MathPersoBase
+{
+    public override async Task Factorielle(Nombre request, IServerStreamWriter<Resultat> responseStream, ServerCallContext context)
+    {
+        long total = 1;
+        for (int i = 1; i < request.Chiffre; i++)
+        {
+            total *= i;
+
+            // equivaux a un return du stream vers le client
+            await responseStream.WriteAsync(new Resultat
+            {
+                Total = total
+            });
+        }
+    }
+}
